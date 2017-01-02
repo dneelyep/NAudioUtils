0b0dcd08d1f5943bc3a60b0814f7e78d4f218cf8
Daniel Neel <dneelyep@gmail.com>
2017-01-02 15:06:57 -0500

    Initial project checkin. Add a human-readable description to MIDI File formats.

---
+++ NAudioUtils/Extensions/MidiFile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NAudio.Midi;
+
+namespace NAudioUtils.Extensions
+{
+    public static class MidiFileExtensions
+    {
+        public static string FileFormatDescription(this MidiFile file)
+        {
+            switch (file.FileFormat)
+            {
+                case 0:
+                    return "Single track";
+                case 1:
+                    return "Multiple tracks";
+                case 2:
+                    return "Multiplie songs";
+                default:
+                    throw new ArgumentOutOfRangeException($"Invalid MIDI file format provided: {file.FileFormat}");
+            }
+        }
+
+    }
+}
